808fbc7e1fc83415c3c825836171e305dff7084a
Masahide Kojima <mk19830920@gmail.com>
2026-01-09 17:39:20 +0900

    fix: change cloud function region to us-central1 to fix hosting rewrites

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import type { Express, Request, Response } from "express";
 // Note: Server is bundled by esbuild into lib/server/index.js
 // This import works after running `pnpm build:functions`
 export const api = onRequest({
-    region: "asia-northeast1",
+    region: "us-central1",
     timeoutSeconds: 60,
     memory: "1GiB",
     cors: true,
